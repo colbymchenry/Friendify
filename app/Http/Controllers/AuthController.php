92de75d15b54a2671239058613f4295c0483bf1f
colbymchenry <colbymchenry@gmail.com>
2018-06-12 15:14:27 -0400

    Didn't do anything

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ class AuthController extends Controller
 
   function register(\Request $request)
   {
+    \Log::info("CINNAMON");
       \DB::statement("CREATE TABLE IF NOT EXISTS users (firstname TEXT, lastname TEXT, email TEXT, password TEXT, dob TEXT, gender BOOLEAN)");
       return response()->json(['success'=>'Data is successfully added']);
   }
