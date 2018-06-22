6e6a22992b3bdbc9467e7e2fc837fa484b1ae9a3
colbymchenry <colbymchenry@gmail.com>
2018-06-22 13:59:31 -0400

    Small change to use User model.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,23 +16,17 @@ class SearchController extends Controller {
       $results = \DB::table('users')->get()->toArray();
       $limit = min(count($results) - 1, $request['loaded'] + $request['to_load']);
       $output = array();
-<<<<<<< HEAD
-      for ($i = 0; $i < $limit; $i++) {
-        $user = User::where('uuid', $results[$i]->uuid);
-        array_push($output, $user->to_array());
-=======
       $index = 0;
-      $user = new \App\User($request['uuid']);
+      $user = User::where('uuid', $request['uuid'])->get()->first();
       while ($loaded < $limit && $index < count($results)) {
         if ($results[$index]->uuid != $request['uuid']) {
-          $match = new \App\User($results[$index]->uuid);
+          $match = User::where('uuid', $results[$index]->uuid)->get()->first();
           if ($user->match_score_with($match) > 0) {
             $loaded++;
             array_push($output, $match->to_array());
           }
         }
         $index++;
->>>>>>> f71cdf06965a1b17e2922b922ab68944a7b61131
       }
       return response()->json(['output' => $output, 'loaded' => $loaded]);
     } catch (Exception $e) {
